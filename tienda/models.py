a59bd004857aed1c7b9674fdc1995a3492c3f1ab
Fafuenzali <82009326+Fafuenzali@users.noreply.github.com>
2021-07-19 09:57:50 -0400

    Login y logout y primeros modelos

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    sku = models.CharField(max_length=7, null=False, unique=True, verbose_name='Sku')
+    nombre = models.CharField(max_length=255, null=False, unique=True, verbose_name='nombre')
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+        ordering = ['id']
--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import login
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+
+
+
 
 class VistaRegistro(View):
     def get(self, request):
@@ -21,8 +23,39 @@ class VistaRegistro(View):
         else:
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
-            return render(request, "registro.html", {"form": form})
+            return render(request, "registro", {"form": form})
+
+def acceder(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request, f"Bienvenido/a nuevamente {nombre}")
+                return redirect("base")
+            else:
+                messages.error(request, "Los datos son incorrectos")
+        else: 
+            messages.error(request, "Los datos son incorrectos")       
+    form = AuthenticationForm()
+    return render(request, "acceder.html", {"form": form})
+
+def salir(request):
+    logout(request)
+    messages.success(request, F"Sesión finalizada")
+    return redirect("acceder")
+
+
+
+
 
 def base(request):
     return render(request, "base.html")
+
+def productos (request):
+    return render(request, "productos.html")
+
 
